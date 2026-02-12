2015db0465f3637515b9af2fe1b699e5de96556a
tobyrushton <tobyjrushton@icloud.com>
2026-02-12 13:45:34 +0000

    search: redfine constructor

--- packages/search/search.go
+++ packages/search/search.go
@@ -3,23 +3,20 @@ package search
 import (
 	"context"
 
-	"github.com/tobyrushton/gflights"
+	"github.com/tobyrushton/flyvia/packages/search/provider"
 )
 
 type Search struct {
 	ctx context.Context
-
-	s *gflights.Session
+	p   provider.Provider
 }
 
-func New(ctx context.Context) (*Search, error) {
-	s, err := gflights.New()
-	if err != nil {
-		return nil, err
-	}
-
+func New(
+	ctx context.Context,
+	provider provider.Provider,
+) *Search {
 	return &Search{
 		ctx: ctx,
-		s:   s,
-	}, nil
+		p:   provider,
+	}
 }
